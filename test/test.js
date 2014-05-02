1dbefd405c8874e6976d81bf92d2a26aefa94ef1
Lance Stout <lancestout@gmail.com>
2014-05-02 15:36:00 -0700

    Update linting

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var test = require('tape');
 var jxt = require('jxt');
@@ -7,57 +7,57 @@ var XRD = require('../lib/xrd');
 var getHostMeta = require('../index');
 
 
-var xml = "<?xml version='1.0' encoding='UTF-8'?>" +
-"<XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0'" +
-"     xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'>" +
-"  <Subject>http://blog.example.com/article/id/314</Subject>" +
-"  <Expires>2010-01-30T09:30:00Z</Expires>" +
-"  <Alias>http://blog.example.com/cool_new_thing</Alias>" +
-"  <Alias>http://blog.example.com/steve/article/7</Alias>" +
-"  <Property type='http://blgx.example.net/ns/version'>1.2</Property>" +
-"  <Property type='http://blgx.example.net/ns/version'>1.3</Property>" +
-"  <Property type='http://blgx.example.net/ns/ext' xsi:nil='true' />" +
-"  <Link rel='author' type='text/html'" +
-"        href='http://blog.example.com/author/steve'>" +
-"    <Title>About the Author</Title>" +
-"    <Title xml:lang='en-us'>Author Information</Title>" +
-"    <Property type='http://example.com/role'>editor</Property>" +
-"  </Link>" +
-"  <Link rel='author' href='http://example.com/author/john'>" +
-"    <Title>The other guy</Title>" +
-"    <Title>The other author</Title>" +
-"  </Link>" +
-"</XRD>";
+var xml = '<?xml version="1.0" encoding="UTF-8"?>' +
+'<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0"' +
+'     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">' +
+'  <Subject>http://blog.example.com/article/id/314</Subject>' +
+'  <Expires>2010-01-30T09:30:00Z</Expires>' +
+'  <Alias>http://blog.example.com/cool_new_thing</Alias>' +
+'  <Alias>http://blog.example.com/steve/article/7</Alias>' +
+'  <Property type="http://blgx.example.net/ns/version">1.2</Property>' +
+'  <Property type="http://blgx.example.net/ns/version">1.3</Property>' +
+'  <Property type="http://blgx.example.net/ns/ext" xsi:nil="true" />' +
+'  <Link rel="author" type="text/html"' +
+'        href="http://blog.example.com/author/steve">' +
+'    <Title>About the Author</Title>' +
+'    <Title xml:lang="en-us">Author Information</Title>' +
+'    <Property type="http://example.com/role">editor</Property>' +
+'  </Link>' +
+'  <Link rel="author" href="http://example.com/author/john">' +
+'    <Title>The other guy</Title>' +
+'    <Title>The other author</Title>' +
+'  </Link>' +
+'</XRD>';
 
 var json = {
-    "subject": "http://blog.example.com/article/id/314",
-    "expires": "2010-01-30T09:30:00.000Z",
-    "aliases": [
-        "http://blog.example.com/cool_new_thing",
-        "http://blog.example.com/steve/article/7"
+    'subject': 'http://blog.example.com/article/id/314',
+    'expires': '2010-01-30T09:30:00.000Z',
+    'aliases': [
+        'http://blog.example.com/cool_new_thing',
+        'http://blog.example.com/steve/article/7'
     ],
-    "properties": {
-        "http://blgx.example.net/ns/version": "1.3",
-        "http://blgx.example.net/ns/ext": ""
+    'properties': {
+        'http://blgx.example.net/ns/version': '1.3',
+        'http://blgx.example.net/ns/ext': ''
     },
-    "links": [
+    'links': [
         {
-            "rel": "author",
-            "type": "text/html",
-            "href": "http://blog.example.com/author/steve",
-            "titles": {
-                "default": "About the Author",
-                "en-us": "Author Information"
+            'rel': 'author',
+            'type': 'text/html',
+            'href': 'http://blog.example.com/author/steve',
+            'titles': {
+                'default': 'About the Author',
+                'en-us': 'Author Information'
             },
-            "properties": {
-                "http://example.com/role": "editor"
+            'properties': {
+                'http://example.com/role': 'editor'
             }
         },
         {
-            "rel": "author",
-            "href": "http://example.com/author/john",
-            "titles": {
-                "default": "The other author"
+            'rel': 'author',
+            'href': 'http://example.com/author/john',
+            'titles': {
+                'default': 'The other author'
             }
         }
     ]
@@ -116,7 +116,7 @@ test('missing host-meta', function (t) {
         host: 'dne.lance.im',
         json: true,
         xrd: true
-    }, function (err, hostmeta) {
+    }, function (err) {
         t.equal(err, 'no-host-meta');
         t.end();
     });
